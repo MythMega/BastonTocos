efce84e6a453bc8c88272c0efad92600d5a03ba6
MythMega <mythmegass@gmail.com>
2026-01-20 19:21:32 +0100

    Routes de bases

---
+++ Entity/Match/AssautItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bastocos.Entity.Match
+{
+    internal class AssautItem
+    {
+    }
+}
